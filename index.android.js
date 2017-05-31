bbbe57c324e71a437da5bc9a0b10925833255ddb
neetu <neetuy1455@gmail.com>
2017-05-31 16:09:20 +0530

    login page and & database added

--- index.android.js
+++ index.android.js
@@ -3,6 +3,8 @@
  * https://github.com/facebook/react-native
  * @flow
  */
+var React = require('react-native');
+var SQLite = require('react-native-sqlite-storage')
 
 import React, { Component } from 'react';
 import {
@@ -11,16 +13,24 @@ import {
   Text,
   View
 } from 'react-native';
-import Example from './Example';
 import Login from './src/Components/Login/Login'
+
+
 export default class ReactNativeProject extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      record: null
+    }
+                                                                                              
+    let db = SQLite.openDatabase({name: 'example.db', createFromLocation : "~example.openDatabase", location: 'Library'}, this.openCB, this.errorCB);
+    console.log(db);
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-       
-        <Example />
-        <Login />
-      </View>
+      <Login />
     );
   }
 }
