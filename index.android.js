ab0b21e35721de07565f077c388358bcf03cc54e
neetu <neetuy1455@gmail.com>
2017-06-12 12:56:49 +0530

    grid in homePage

--- index.android.js
+++ index.android.js
@@ -1,31 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-// import React, { Component } from 'react';
-// import {
-//   AppRegistry,
-//   Alert,
-// } from 'react-native';
-// import Login from './src/Components/Login/Login';
 
-// // import ReactNativeLogin from './App/components/App';
-
-// class ReactNativeProject extends Component {
-
-	
-
-//   render(){
-//     return (
-//       <Login
-       
-//       />
-//     );
-//   }
-// }
-
-// AppRegistry.registerComponent('ReactNativeProject', () =>ReactNativeProject);
 import {
   AppRegistry,
   Alert,
@@ -48,3 +21,5 @@ export default class ReactNativeProject extends Component {
 }
 
 	AppRegistry.registerComponent('ReactNativeProject', () =>ReactNativeProject);
+
+
--- src/Components/Home/HomePage.js
+++ src/Components/Home/HomePage.js
@@ -1,50 +1,82 @@
-import React from 'react'
-import { TouchableOpacity, Text, View, AsyncStorage } from 'react-native';
-import { Actions } from 'react-native-router-flux';
+// import React from 'react'
+// import { TouchableOpacity, Text, View, AsyncStorage } from 'react-native';
+// import { Actions } from 'react-native-router-flux';
 
-const Home = () => {
+// const Home = () => {
 
-    setData = (value) => {
-      AsyncStorage.setItem('email', JSON.stringify(value))
-       AsyncStorage.setItem('password', JSON.stringify(value))
-    }
+//     setData = (value) => {
+//       AsyncStorage.setItem('email', JSON.stringify(value))
+//        AsyncStorage.setItem('password', JSON.stringify(value))
+//     }
    
-    getData = (value) => {
-      AsyncStorage.getItem('email').then((value) => {
-        var email = JSON.parse(value); // boolean false
-        alert(email);
-      });
-      AsyncStorage.getItem('password').then((value) => {
-        var password = JSON.parse(value); // boolean false
-        alert(password);
-      });
+//     getData = (value) => {
+//       AsyncStorage.getItem('email').then((value) => {
+//         var email = JSON.parse(value); // boolean false
+//         alert(email);
+//       });
+//       AsyncStorage.getItem('password').then((value) => {
+//         var password = JSON.parse(value); // boolean false
+//         alert(password);
+//       });
 
-    }
-    deleteItem = (value) => {
-      AsyncStorage.removeItem('email').then((value) => {
-          var email = JSON.parse(value); // boolean false
-          alert(email);
-      });
-      AsyncStorage.removeItem('password').then((value) => {
-          var password = JSON.parse(value); // boolean false
-          alert(password);
-      });
+//     }
+//     deleteItem = (value) => {
+//       AsyncStorage.removeItem('email').then((value) => {
+//           var email = JSON.parse(value); // boolean false
+//           alert(email);
+//       });
+//       AsyncStorage.removeItem('password').then((value) => {
+//           var password = JSON.parse(value); // boolean false
+//           alert(password);
+//       });
 
-    }
-   return (
-    <View>
-      <View>
-        <TouchableOpacity style = {{ margin: 128 }} onPress={this.getData}>
-           <Text>press</Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <TouchableOpacity style = {{ margin: 128 }} onPress={this.deleteItem}>
-           <Text>delet</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-   )
+//     }
+//    return (
+//     <View>
+//       <View>
+//         <TouchableOpacity style = {{ margin: 128 }} onPress={this.getData}>
+//            <Text>press</Text>
+//         </TouchableOpacity>
+//       </View>
+//       <View>
+//         <TouchableOpacity style = {{ margin: 128 }} onPress={this.deleteItem}>
+//            <Text>delet</Text>
+//         </TouchableOpacity>
+//       </View>
+//     </View>
+//    )
+// }
+
+// export default Home
+
+import React from 'react';
+import { View, ListView, StyleSheet, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+  },
+});
+
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
+  }
+  render() {
+    return (
+      <ListView
+        style={styles.container}
+        dataSource={this.state.dataSource}
+        renderRow={(data) => <View><Text>{data}</Text></View>}
+      />
+    );
+  }
 }
 
-export default Home
+export default HomePage;
